7d05e776529e1debfeb0b245b0869b7e177d62d9
dikshu-catalyst <156075007+dikshu-catalyst@users.noreply.github.com>
2024-02-05 11:06:26 +0530

    Update result.php

--- ct_project/result.php
+++ ct_project/result.php
@@ -1,17 +1,26 @@
 <?php
+include 'data.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") 
 {
+   
     $userId = $_POST['userId'];
     $password = $_POST['password'];
+  
 
-    $output = [
-        'userId' => $userId,
-        'password' => $password,
-    ];
+    if (isset($_FILES['upload_file'])) {
+        $image = $_FILES['upload_file']['name'];
+        $file_tmp = $_FILES['upload_file']['tmp_name'];
+        move_uploaded_file($file_tmp, "assets/uploads/" . $image);
+    }
 
-    header('Content-Type: application/json');
-    
-    echo json_encode($output);
-    //exit; Stop execution after sending JSON response
+    $sql = "INSERT INTO image (userId,password,image)
+    VALUES ('$userId','$password','$image')";
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+       echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+
 }
 ?>
