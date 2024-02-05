37ccef86f20d2018d545aa3e6c373ce4084adfda
dikshu-catalyst <156075007+dikshu-catalyst@users.noreply.github.com>
2024-02-05 11:07:26 +0530

    Update index.js

--- ct_project/assets/js/index.js
+++ ct_project/assets/js/index.js
@@ -1,19 +1,25 @@
 $(document).ready(function(){
     $("#submit-button").click(function () {
+        var id = $("#id").val();
         var userId = $("#userId").val();
         var password = $("#password").val();
+
+        var formData = new FormData();
+       formData.append("id", id);
+       formData.append("userId",userId);
+       formData.append("password",password);
+
+       var fileInput = $("#upload_file")[0].files[0];
+       formData.append("upload_file",fileInput);
+
         $.ajax({
             type: 'POST',
             url: 'result.php',
-            data: {
-                userId: userId,
-                password: password,
-            },
+            data: formData,
+            contentType: false,
+            processData : false,  
             success: function(response) {
-                var $final = response.userId+" "+response.password;
-                // Display values
-                alert($final);
-
+                alert(response);
             }
         });
     });
