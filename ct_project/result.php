6f2dcdfd368ce1f18fcc689f54b76a5a6d36c2fd
dikshu-catalyst <156075007+dikshu-catalyst@users.noreply.github.com>
2024-02-05 14:31:08 +0530

    Update result.php

--- ct_project/result.php
+++ ct_project/result.php
@@ -13,14 +13,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
         move_uploaded_file($file_tmp, "assets/uploads/" . $image);
     }
 
-    $sql = "INSERT INTO image (userId,password,image)
-    VALUES ('$userId','$password','$image')";
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-    } else {
-       echo "Error: " . $sql . "<br>" . $conn->error;
+    // Check if userId already exists
+    $checkUserId = "SELECT * FROM image WHERE userId = '$userId'";
+    $result = $conn->query($checkUserId);
+
+
+    if($result->num_rows>0){
+        $update = "UPDATE image  SET password = '$password', image = '$image'  WHERE userId='$userId'";
+        if($conn->query($update) === TRUE)
+        {
+            echo "Update Successfully";
+        }
+        else
+        {
+            echo  "Error: "  . $conn->error;
+        }
+    }
+    else{
+        $sql = "INSERT INTO image (userId,password,image)
+         VALUES ('$userId','$password','$image')";
+         if ($conn->query($sql) === TRUE) 
+            {
+                 echo "New record created successfully";
+             } 
+         else 
+            {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
     }
 
-
+    // $sql = "INSERT INTO image (userId,password,image)
+    // VALUES ('$userId','$password','$image')";
+    // if ($conn->query($sql) === TRUE) {
+    //     echo "New record created successfully";
+    // } else {
+    //    echo "Error: " . $sql . "<br>" . $conn->error;
+    // }
+    
 }
 ?>
